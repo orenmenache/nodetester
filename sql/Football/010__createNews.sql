737c9575590ff9a3cf9bc951c3107dc209941f48
Oren <oren.menache@gmail.com>
2024-01-22 21:21:54 +0200

    create news

---
+++ sql/Football/010__createNews.sql
@@ -0,0 +1,11 @@
+USE Football;
+DROP TABLE IF EXISTS RAPID__NEWS;
+CREATE TABLE RAPID__NEWS (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    headline VARCHAR(255) NOT NULL,
+    subheadline VARCHAR(255) NOT NULL,
+    narration TEXT NOT NULL,
+    when_created TIMESTAMP,
+    background VARCHAR(255),
+    logo VARCHAR(255)
+);
